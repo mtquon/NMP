eb5b597e8ca2bb1cc325c86b72195b7f1001e078
mtquon <mitchellzero@gmail.com>
2020-10-09 16:48:09 -0700

    Migrated from ViewPager to ViewPager2

--- app/src/main/java/com/example/qusic/MainActivity.kt
+++ app/src/main/java/com/example/qusic/MainActivity.kt
@@ -8,20 +8,32 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.viewpager2.widget.ViewPager2
 import com.example.qusic.ui.main.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class MainActivity : AppCompatActivity() {
-
+    private val TAB_TITLES = arrayOf(
+        R.string.tab_songs,
+        R.string.tab_playlist,
+        R.string.tab_albums,
+        R.string.tab_artists
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
+
+
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        TabLayoutMediator(tabs, viewPager) {tab, position ->
+            tab.text = this.resources.getString(TAB_TITLES[position])
+        }.attach()
         val fab: FloatingActionButton = findViewById(R.id.fab)
-
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
--- app/src/main/java/com/example/qusic/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/qusic/ui/main/SectionsPagerAdapter.kt
@@ -2,38 +2,34 @@ package com.example.qusic.ui.main
 
 import android.content.Context
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.qusic.R
 import com.example.qusic.fragments.PlaceholderFragment
 
-private val TAB_TITLES = arrayOf(
-        R.string.tab_songs,
-        R.string.tab_playlist,
-        R.string.tab_albums,
-        R.string.tab_artists
-)
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
-    : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(private val context: Context, fa:FragmentActivity) : FragmentStateAdapter(fa){
 
-    override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
+
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun createFragment(position: Int): Fragment{
+        // createFragment is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
         return PlaceholderFragment.newInstance(position + 1)
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
 
-    override fun getCount(): Int {
-        // Show 4 total pages.
-        return 4
-    }
+
 }
+
+
 
